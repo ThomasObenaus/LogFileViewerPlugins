34d2b2c67279937c7de9d4173d3304878f577ba9
Thomas Obenaus <obenaus.thomas@gmail.com>
2015-03-07 18:10:26 +0100

    .ADD Console plugin added

---
+++ src/thobe/logfileViewer/plugins/console/events/CEvtClear.java
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (C) 2014, Thomas Obenaus. All rights reserved.
+ *  Licensed under the New BSD License (3-clause lic)
+ *  See attached license-file.
+ *
+ *	Author: 	Thomas Obenaus
+ *	EMail:		obenaus.thomas@gmail.com
+ *  Project:    LogFileViewer
+ */
+
+package thobe.logfileViewer.plugins.console.events;
+
+/**
+ * @author Thomas Obenaus
+ * @source CEvtClear.java
+ * @date 25.06.2014
+ */
+public class CEvtClear extends ConsoleEvent
+{
+	public CEvtClear( )
+	{
+		super( ConsoleEventType.CLEAR );
+	}
+}
