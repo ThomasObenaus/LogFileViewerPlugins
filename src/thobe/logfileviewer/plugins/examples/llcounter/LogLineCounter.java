4e17bb6e5955121eed687256134b71996b7ed497
Thomas Obenaus <obenaus.thomas@gmail.com>
2015-04-12 23:35:58 +0200

    .UPDATE example LogLineCounter shrinked according to new api

--- src/thobe/logfileviewer/plugins/examples/llcounter/LogLineCounter.java
+++ src/thobe/logfileviewer/plugins/examples/llcounter/LogLineCounter.java
@@ -1,10 +1,8 @@
 package thobe.logfileviewer.plugins.examples.llcounter;
+
 import java.util.List;
-import java.util.regex.Pattern;
 
 import thobe.logfileviewer.plugin.Plugin;
-import thobe.logfileviewer.plugin.api.IPluginAccess;
-import thobe.logfileviewer.plugin.api.IPluginUIComponent;
 import thobe.logfileviewer.plugin.source.logline.ILogLine;
 
 /*
@@ -53,18 +51,6 @@ public class LogLineCounter extends Plugin
 	}
 
 	@Override
-	public String getPluginDescription( )
-	{
-		return "Plugin counting log-lines";
-	}
-
-	@Override
-	public String getNameOfMemoryWatchable( )
-	{
-		return "LogLineCounter";
-	}
-
-	@Override
 	public long getMemory( )
 	{
 		// nothing to do since we don't consume mem
@@ -77,79 +63,20 @@ public class LogLineCounter extends Plugin
 		// nothing to do since we don't consume mem
 	}
 
-	/* Configuration methods */
-	@Override
-	public Pattern getLineFilter( )
-	{
-		// return null to get all log-lines,
-		// use a pattern to get only specific log-lines
-		return null;
-	}
-
-	@Override
-	public IPluginUIComponent getUIComponent( )
-	{
-		// just return null since we have no gui
-		return null;
-	}
-
 	/* Methods for obtaining the log-lines*/
 	@Override
 	public void onNewBlockOfLines( List<ILogLine> blockOfLines )
 	{
 		// here we can count and print out the number of lines
-		this.numLogLines+=blockOfLines.size( );
+		this.numLogLines += blockOfLines.size( );
 		System.out.println( "#LLs=" + this.numLogLines );
 	}
 
-	/* Plugin life-cycle*/
-	@Override
-	public boolean onStarted( )
-	{
-		// called on starting the plugin 
-		// other plugins might no be available at this point
-		return true;
-	}
-
-	@Override
-	public boolean onRegistered( IPluginAccess pluginAccess )
-	{
-		// called when this plugin is registered 
-		// other plugins are available at this point and can be
-		// referenced via IPluginAccess
-		return true;
-	}
-
+	/* Plugin life-cycle methods*/
 	@Override
 	public void onLogStreamOpened( )
 	{
 		// called whenever a new logstream (file or over ip) is opened
 		this.numLogLines = 0;
 	}
-
-	@Override
-	public void onPrepareCloseLogStream( )
-	{
-		// called whenever the current logstream will be closed
-	}
-
-	@Override
-	public void onLogStreamClosed( )
-	{
-		// called whenever the current logstream is closed
-	}
-
-	@Override
-	public void onUnRegistered( )
-	{
-		// called when this plugin is unregistered
-	}
-
-	@Override
-	public boolean onStopped( )
-	{
-		// called when this plugin is stopped
-		return true;
-	}
-
 }
